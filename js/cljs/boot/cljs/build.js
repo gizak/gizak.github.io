c91470dd280c6ccaeb14293f03dbc5fe3c30693a
gizak <gizak@icloud.com>
2015-12-15 01:58:02 -0500

    Rebuild

--- js/cljs/boot/cljs/build.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 0.0-3196 {:static-fns true, :optimize-constants true, :elide-asserts true}
-goog.provide('boot.cljs.build');
-goog.require('cljs.core');
-goog.require('gio.command');
-goog.require('gio.core');
